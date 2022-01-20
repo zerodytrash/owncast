1b9d8f7d0ca9f3384622c0c58510e6afcc5f4e8d
Gabe Kangas <gabek@real-ity.com>
2022-01-20 14:00:00 -0800

    Email notifications/smtp support

--- webroot/js/utils/constants.js
+++ webroot/js/utils/constants.js
@@ -19,6 +19,7 @@ export const URL_CHAT_REGISTRATION = `/api/chat/register`;
 export const URL_FOLLOWERS = `/api/followers`;
 
 export const URL_REGISTER_NOTIFICATION = `/api/notifications/register`;
+export const URL_REGISTER_EMAIL_NOTIFICATION = `/api/notifications/register/email`;
 
 export const TIMER_STATUS_UPDATE = 5000; // ms
 export const TIMER_DISABLE_CHAT_AFTER_OFFLINE = 5 * 60 * 1000; // 5 mins
